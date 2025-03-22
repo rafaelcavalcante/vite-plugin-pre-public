ebc9e0e1420459eeef0ebba1fb807329f41de94e
Rafael Cavalcante <eu@rafaelcavalcante.com>
2025-03-22 13:34:39 -0300

    chore: remove dist folder

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Plugin } from "vite";
-
-declare interface ParseFromPrePublicOptions {
-  files: string[];
-}
-
-declare function parseFromPrePublic(options: ParseFromPrePublicOptions): Plugin;
-
-export { parseFromPrePublic };
